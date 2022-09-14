2124d45c9df436675121d1b810bdecf807b1026b
phatld <phatld@fireapps.vn>
2022-09-14 18:01:17 +0700

    update

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        dd('product api');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        dd('product show api');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\ProvisionServer;
+use App\Http\Controllers\VersionController;
+use App\Http\Middleware\EnsureTokenIsValid;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +21,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+
+// Route::get('/photos', [PhotoController::class, 'index']);
+// Route::get('/photos/create', [PhotoController::class, 'create']);
+// Route::post('/photos', [PhotoController::class, 'store']);
+// Route::get('/photos/{photo}', [PhotoController::class, 'show']);
+// Route::get('/photos/{photo}/edit', [PhotoController::class, 'edit']);
+// Route::put('/photos/{photo}', [PhotoController::class, 'update']);
+// Route::delete('/photos/{photo}', [PhotoController::class, 'destroy']);
+
+Route::resource('photos', PhotoController::class)
+    ->missing(function (Request $request) {
+        return Redirect::route('photos.index');
+    });
+
+Route::get('/server', ProvisionServer::class);
+
+Route::controller(VersionController::class)->prefix('version')->name('version.')->group(function () {
+    Route::get('/test', 'index')->middleware('token');
 });
